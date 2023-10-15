a05edf48b69722c0dc3a320fe500841656ed8402
caunhach <caunhach@LAPTOP-5U4J17VN.localdomain>
2023-10-15 15:02:16 +0700

    last updated 15/10/2023

--- src/calculatorapplication/CalculatorApplication.java
+++ src/calculatorapplication/CalculatorApplication.java
@@ -4,17 +4,254 @@
  */
 package calculatorapplication;
 
-/**
- *
- * @author acer
- */
-public class CalculatorApplication {
+import java.awt.*;
+import java.awt.event.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+import javax.swing.*;
+
+public class CalculatorApplication implements ActionListener{
+
+    double num1=0, num2=0, result=0;
+    int calculation;
+    JFrame frame = new JFrame("Calculator");
+    JLabel label = new JLabel();
+    JTextField textField = new JTextField();
+    JRadioButton onRadioButton = new JRadioButton("on");
+    JRadioButton offRadioButton = new JRadioButton("off");
+    JButton buttonZero = new JButton("0");
+    JButton buttonOne = new JButton("1");
+    JButton buttonTwo = new JButton("2");
+    JButton buttonThree = new JButton("3");
+    JButton buttonFour = new JButton("4");
+    JButton buttonFive = new JButton("5");
+    JButton buttonSix = new JButton("6");
+    JButton buttonSeven = new JButton("7");
+    JButton buttonEight = new JButton("8");
+    JButton buttonNine = new JButton("9");
+    JButton buttonDot = new JButton(".");
+    JButton buttonClear = new JButton("C");
+    JButton buttonDelete = new JButton("DEL");
+    JButton buttonEqual = new JButton("=");
+    JButton buttonMUL = new JButton("X");
+    JButton buttonDiv = new JButton("/");
+    JButton buttonPLUS = new JButton("+");
+    JButton buttonMINUS = new JButton("-");
+    JButton buttonSQUARE = new JButton("X\u00B2");
+    JButton buttonReciprocal = new JButton("1/X");
+    JButton buttonSQRT = new JButton("\u221A");
+    CalculatorApplication(){
+        prepareGui();
+        addComponents();
+        addActionEvent();
     }
-    
+    public void prepareGui(){
+        frame.setSize(305, 510);
+        frame.getContentPane().setLayout(null);
+        frame.getContentPane().setBackground(Color.black);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void addComponents(){
+
+        label.setBounds(250, 0, 50, 0);
+        label.setForeground(Color.white);
+        frame.add(label);
+
+        textField.setBounds(10,40,270,40);
+        textField.setFont(new Font("Arial",Font.BOLD,20));
+        textField.setEditable(false);
+        textField.setHorizontalAlignment(SwingConstants.RIGHT);
+        frame.add(textField);
+
+        onRadioButton.setBounds(10, 95, 60, 40);
+        onRadioButton.setSelected(true);
+        onRadioButton.setFont(new Font("Arial", Font.BOLD, 14));
+        onRadioButton.setBackground(Color.black);
+        onRadioButton.setForeground(Color.white);
+        onRadioButton.setFocusable(false);
+        frame.add(onRadioButton);
+
+        offRadioButton.setBounds(10, 120, 60, 40);
+        offRadioButton.setSelected(false);
+        offRadioButton.setFont(new Font("Arial", Font.BOLD, 14));
+        offRadioButton.setBackground(Color.black);
+        offRadioButton.setForeground(Color.white);
+        offRadioButton.setFocusable(false);
+        frame.add(offRadioButton);
+
+        ButtonGroup buttonGroup = new ButtonGroup();
+        buttonGroup.add(onRadioButton);
+        buttonGroup.add(offRadioButton);
+
+        buttonSeven.setBounds(10, 230, 60, 40);
+        buttonSeven.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonSeven.setFocusable(false);
+        frame.add(buttonSeven);
+
+        buttonEight.setBounds(80, 230, 60, 40);
+        buttonEight.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonEight.setFocusable(false);
+        frame.add(buttonEight);
+
+        buttonNine.setBounds(150, 230, 60, 40);
+        buttonNine.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonNine.setFocusable(false);
+        frame.add(buttonNine);
+        
+        buttonFour.setBounds(10, 290, 60, 40);
+        buttonFour.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonFour.setFocusable(false);
+        frame.add(buttonFour);
+
+        buttonFive.setBounds(80, 290, 60, 40);
+        buttonFive.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonFive.setFocusable(false);
+        frame.add(buttonFive);
+
+        buttonSix.setBounds(150, 290, 60, 40);
+        buttonSix.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonSix.setFocusable(false);
+        frame.add(buttonSix);
+
+        buttonOne.setBounds(10, 350, 60, 40);
+        buttonOne.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonOne.setFocusable(false);
+        frame.add(buttonOne);
+
+        buttonTwo.setBounds(80, 350, 60, 40);
+        buttonTwo.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonTwo.setFocusable(false);
+        frame.add(buttonTwo);
+
+        buttonThree.setBounds(150, 350, 60, 40);
+        buttonThree.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonThree.setFocusable(false);
+        frame.add(buttonThree);
+
+        buttonDot.setBounds(150, 410, 60, 40);
+        buttonDot.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonDot.setFocusable(false);
+        frame.add(buttonDot);
+
+        buttonZero.setBounds(10, 410, 130, 40);
+        buttonZero.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonZero.setFocusable(false);
+        frame.add(buttonZero);
+
+        buttonEqual.setBounds(220, 350, 60, 100);
+        buttonEqual.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonEqual.setFocusable(false);
+        frame.add(buttonEqual);
+
+        buttonDiv.setBounds(220, 110, 60, 40);
+        buttonDiv.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonDiv.setFocusable(false);
+        frame.add(buttonDiv);
+
+        buttonSQRT.setBounds(10, 170, 60, 40);
+        buttonSQRT.setFont(new Font("Arial", Font.BOLD, 16));
+        buttonSQRT.setFocusable(false);
+        frame.add(buttonSQRT);
+
+        buttonMUL.setBounds(220, 230, 60, 40);
+        buttonMUL.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMUL.setFocusable(false);
+        frame.add(buttonMUL);
+
+        buttonMINUS.setBounds(220, 170, 60, 40);
+        buttonMINUS.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonMINUS.setFocusable(false);
+        frame.add(buttonMINUS);
+
+        buttonPLUS.setBounds(220, 290, 60, 40);
+        buttonPLUS.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonPLUS.setFocusable(false);
+        frame.add(buttonPLUS);
+
+        buttonSQUARE.setBounds(80, 170, 60, 40);
+        buttonSQUARE.setFont(new Font("Arial", Font.BOLD, 16));
+        buttonSQUARE.setFocusable(false);
+        frame.add(buttonSQUARE);
+
+        buttonReciprocal.setBounds(150, 170, 60, 40);
+        buttonReciprocal.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonReciprocal.setFocusable(false);
+        frame.add(buttonReciprocal);
+
+        buttonDelete.setBounds(150, 110, 60, 40);
+        buttonDelete.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonDelete.setBackground(Color.red);
+        buttonDelete.setForeground(Color.white);
+        buttonDelete.setFocusable(false);
+        frame.add(buttonDelete);
+
+        buttonClear.setBounds(80, 110, 60, 40);
+        buttonClear.setFont(new Font("Arial", Font.BOLD, 12));
+        buttonClear.setBackground(Color.red);
+        buttonClear.setForeground(Color.white);
+        buttonClear.setFocusable(false);
+        frame.add(buttonClear);
+    }
+
+    public void addActionEvent(){
+
+        onRadioButton.addActionListener(this);
+        offRadioButton.addActionListener(this);
+        buttonClear.addActionListener(this);
+        buttonDelete.addActionListener(this);
+        buttonDiv.addActionListener(this);
+        buttonSQRT.addActionListener(this);
+        buttonSQUARE.addActionListener(this);
+        buttonReciprocal.addActionListener(this);
+        buttonMINUS.addActionListener(this);
+        buttonSeven.addActionListener(this);
+        buttonEight.addActionListener(this);
+        buttonNine.addActionListener(this);
+        buttonMUL.addActionListener(this);
+        buttonFour.addActionListener(this);
+        buttonFive.addActionListener(this);
+        buttonSix.addActionListener(this);
+        buttonPLUS.addActionListener(this);
+        buttonOne.addActionListener(this);
+        buttonTwo.addActionListener(this);
+        buttonThree.addActionListener(this);
+        buttonEqual.addActionListener(this);
+        buttonZero.addActionListener(this);
+        buttonDot.addActionListener(this);
+    }
+    public static void main(String[] args) {
+        CalculatorApplication Calculator = new CalculatorApplication();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        Object source = e.getSource();
+
+        if (source == onRadioButton){
+            enable();
+        }
+    }
+    public void enable(){
+    onRadioButton.setEnabled(false);
+    offRadioButton.setEnabled(true);
+    textField.setEnabled(true);
+    label.setEnabled(true);
+    buttonClear.setEnabled(true);
+    buttonDelete.setEnabled(true);
+    buttonDiv.setEnabled(true);
+    buttonSQRT.setEnabled(true);
+    buttonSQUARE.setEnabled(true);
+    buttonReciprocal.setEnabled(true);
+    buttonMINUS.setEnabled(true);
+    buttonSeven.setEnabled(true);
+    buttonEight.setEnabled(true);
+    buttonNine.setEnabled(true);
+    buttonFour.setEnabled(true);
+    buttonFive.setEnabled(true);
+    buttonSix.setEnabled(true);
+    buttonPLUS.setEnabled(true);
+    buttonOne.setEnabled(true);
+    buttonTwo.setEnabled(true);
+    buttonThree.setEnabled(true);
+    }    
 }
